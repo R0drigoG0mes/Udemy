93072a76fcb6fca8aa909d68a21bc10ac4b32f24
Rodrigo Gomes Cordeiro <98043205+R0drigoG0mes@users.noreply.github.com>
2022-03-31 17:14:10 -0300

    Estudos dia 31/03/2022

--- teste_c#/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-int k = 9;
-
-Console.WriteLine(k+2);
--- teste_c#/README.md
+++
@@ -1,2 +0,0 @@
-# Udemy
- Todo o material produzido no curso de HTML 5 que realizei no site da Udemy.
